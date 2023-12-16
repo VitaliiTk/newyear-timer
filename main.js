f37437f63642889461833a45284ddfd092004e09
VitaliiTk <77240865+VitaliiTk@users.noreply.github.com>
2023-12-16 21:55:47 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const timerItems = document.querySelectorAll('.timer-item');
+const timerBox = document.querySelector('.timer-box');
+let dateFinish = new Date('Jan 01 2024 00:00:00');
+
+
+function counts() {
+    let nowDate = new Date();
+    let gap = dateFinish - nowDate;
+
+    let days = Math.floor(gap / 1000 / 60 / 60 / 24);
+    let hours = Math.floor(gap / 1000 / 60 / 60) % 24;
+    let minutes = Math.floor(gap / 1000 / 60) % 60;
+    let seconds = Math.floor(gap / 1000) % 60;
+
+    let timerValue = [days, hours, minutes, seconds];
+
+    timerItems.forEach((item, index) => {
+        item.innerHTML = timerValue[index]
+    })
+
+    if (gap < 0) {
+        clearInterval(interval);
+        timerBox.innerHTML = 'Happy New Year!'
+    }
+}
+
+const interval = setInterval(counts, 1000);
+counts()
